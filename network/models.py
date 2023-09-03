938aac230e72462ffc6a7e73bcfb2c03e646aa82
Bilal Salim <bilall1999775@outlook.com>
2023-09-03 16:33:52 +0300

    css problems  commit

--- network/models.py
+++ network/models.py
@@ -10,7 +10,8 @@ class post(models.Model):
      content=models.CharField(max_length=140)
      user=models.ForeignKey(User,on_delete=models.CASCADE,related_name='auther')
      date=models.DateTimeField(auto_now_add=True)
-
+  
+  
      def __str__(self):
         return f"post {self.id} made by {self.user} on {self.date.strftime('%D %B %Y %H:%M:%S')}"
      
