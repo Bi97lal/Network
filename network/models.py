87e3a99dbca5262f72508979db83249a73906706
Bilal Salim <bilall1999775@outlook.com>
2023-09-03 16:22:19 +0300

    Initial commit

---
+++ network/models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+
+class post(models.Model):
+     content=models.CharField(max_length=140)
+     user=models.ForeignKey(User,on_delete=models.CASCADE,related_name='auther')
+     date=models.DateTimeField(auto_now_add=True)
+
+     def __str__(self):
+        return f"post {self.id} made by {self.user} on {self.date.strftime('%D %B %Y %H:%M:%S')}"
+     
+class fllow(models.Model):
+    user_fllower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="person_is_fllowing")
+    user_fllowed = models.ForeignKey(User, on_delete=models.CASCADE, related_name="person_you_fllowed")
+
+    def __str__(self):
+        return f"{self.user_fllower} Following {self.user_fllowed}"
+
+
+
+     
